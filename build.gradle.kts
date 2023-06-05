6d3145275386ad6b06d570637e963f13b68dedc1
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-05 17:00:16 +0330

    add hilt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.serialization)apply false
-    alias(libs.plugins.kotlin.jvm) version "1.8.20" apply false
+    alias(libs.plugins.kotlin.jvm)  apply false
+    alias(libs.plugins.hilt) apply  false
+
 }
