c6ad7c5b72ecf8a283e3d12fd3d341c5612ddf6a
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-05 18:22:09 +0330

    impl Service Object for StoreService

---
+++ core/network/src/main/java/com/core/network/util/consts/Service.kt
@@ -0,0 +1,9 @@
+package com.core.network.util.consts
+
+object Service {
+    const val BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3/"
+    const val CONSUMER_KEY = "consumer_key"
+    const val CONSUMER_KEY_VALUE = "ck_f2b932ec6d4fcaedde0fc24fe630af4830155d45"
+    const val CONSUMER_SECRET = "consumer_secret"
+    const val CONSUMER_SECRET_VALUE = "cs_6426a8487bee81944a0ed4e00799b5d67cfef226"
+}
