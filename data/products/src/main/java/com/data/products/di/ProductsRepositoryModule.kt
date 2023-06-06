b2a95fa58545574c0544d3840658ce7a11ee6c49
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 22:33:44 +0330

    add module for data:product

---
+++ data/products/src/main/java/com/data/products/di/ProductsRepositoryModule.kt
@@ -0,0 +1,38 @@
+package com.data.products.di
+
+import com.core.common.legacy.mappers.ProductDtoToProduct
+import com.core.network.ProductsService
+import com.data.products.data.network.ProductNetworkDataSource
+import com.data.products.data.network.ProductNetworkDataSourceImpl
+import com.data.products.data.repository.ProductRepository
+import com.data.products.data.repository.ProductRepositoryImpl
+import com.example.common_main.network.Dispatcher
+import com.example.common_main.network.KinDispatchers
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ProductsRepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideProductNetworkDataSource(
+        productsService: ProductsService,
+        productDtoToProduct: ProductDtoToProduct,
+        @Dispatcher(KinDispatchers.IO) ioDispatchers: CoroutineDispatcher,
+        @Dispatcher(KinDispatchers.Default) defaultDispatchers: CoroutineDispatcher,
+    ): ProductNetworkDataSource = ProductNetworkDataSourceImpl(
+        productsService, productDtoToProduct, ioDispatchers, defaultDispatchers
+    )
+
+    @Provides
+    @Singleton
+    fun provideProductRepository(productNetworkDataSource: ProductNetworkDataSource): ProductRepository =
+        ProductRepositoryImpl(productNetworkDataSource)
+}
