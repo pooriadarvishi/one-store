832d58e193fb87d652cfebdf8957f00a6b6d72e4
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 22:36:58 +0330

    check response of service

--- feature/home/src/main/java/com/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/feature/home/HomeViewModel.kt
@@ -1,21 +1,21 @@
 package com.feature.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.core.common.enums.common.OrderingFilters
 import com.core.common.model.models.products.ProductsItem
-import com.data.products.data.repository.ProductRepositoryImpl
+import com.data.products.data.repository.ProductRepository
 import com.example.common_main.result.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import javax.inject.Singleton
 
 @HiltViewModel
-class HomeViewModel @Inject @Singleton constructor(
-    private val productRepository: ProductRepositoryImpl,
+class HomeViewModel @Inject constructor(
+    private val productRepository: ProductRepository,
     private val orderingFilters: OrderingFilters
 ) : ViewModel() {
     private val _data = MutableLiveData<Result<List<ProductsItem>>>()
@@ -26,12 +26,15 @@ class HomeViewModel @Inject @Singleton constructor(
         viewModelScope.launch {
             productRepository.getListProducts(
                 1,
-                orderingFilters.orderByDefault(),
+                orderingFilters.orderByPrice(),
                 orderingFilters.orderDefault()
             ).collect {
-                _data.postValue(it)
+                when (it) {
+                    is Result.Error -> Log.e("RESULT", "getData: ${it.exception?.message}")
+                    Result.Loading -> Log.e("RESULT", "getData: loading")
+                    is Result.Success -> Log.e("RESULT", "getData: ${it.data}")
+                }
             }
         }
     }
-
 }
