02b08d94cace9aefdf11050fa401407a1dbaf91f
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-07 00:13:13 +0330

    impl gradle feature category

--- feature/category/build.gradle.kts
+++ feature/category/build.gradle.kts
@@ -1,9 +1,69 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
-    `java-library`
-    kotlin("jvm")
+    id("com.android.library")
+    id("com.google.dagger.hilt.android")
+    kotlin("android")
+    kotlin("kapt")
+}
+android {
+    namespace = "com.feature.category"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    kapt {
+        correctErrorTypes = true
+    }
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+
+
+dependencies {
+
+    implementation(project(":core:common:common-main"))
+    implementation(project(":core:common:model"))
+    implementation(project(":core:common:enums"))
+    implementation(project(":core:common:legacy"))
+    implementation(project(":data:products"))
+
+
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.materia)
+    implementation(libs.androidx.constraintlayout)
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 }
