b88c811b2397c39c7141d705da745ae4b94299c7
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 23:17:07 +0330

    change name MainActivity to DetailsActivity

--- feature/details/src/main/java/com/feature/details/DetailsActivity.kt
+++ feature/details/src/main/java/com/feature/details/DetailsActivity.kt
@@ -2,13 +2,12 @@ package com.feature.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.feature.details.ui.details.DetailsFragment
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
 
     }
 }
