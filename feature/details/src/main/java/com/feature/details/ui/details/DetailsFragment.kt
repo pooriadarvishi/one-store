d71983a4050b4f07986f09a22f0e5fbd44449234
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 23:33:43 +0330

    sort detailsf

--- feature/details/src/main/java/com/feature/details/ui/details/DetailsFragment.kt
+++ feature/details/src/main/java/com/feature/details/ui/details/DetailsFragment.kt
@@ -1,17 +1,15 @@
 package com.feature.details.ui.details
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.feature.details.R
 import com.feature.details.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
-private val detailsViewModel : DetailsViewModel by viewModels()
+    private val detailsViewModel: DetailsViewModel by viewModels()
     private lateinit var detailsBinding: FragmentDetailsBinding
 
 
