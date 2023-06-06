3ed71325b6afbc0542eb81054b1af58a147d2cdc
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 09:28:38 +0330

    refactor ProductsService for  new models

--- core/network/src/main/java/com/core/network/ProductsService.kt
+++ core/network/src/main/java/com/core/network/ProductsService.kt
@@ -1,8 +1,8 @@
 package com.core.network
 
-import com.core.network.model.category.CategoriesDto
+import com.core.network.model.category.CategoriesItemDto
 import com.core.network.model.details.ProductDetailsDto
-import com.core.network.model.products.ProductsDto
+import com.core.network.model.products.ProductsResponseItemDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,32 +11,32 @@ interface ProductsService {
     @GET("products")
     suspend fun getListProducts(
         @Query("page") page: Int, @Query("orderby") orderBy: String
-    ): ProductsDto
+    ): List<ProductsResponseItemDto>
 
     @GET("products")
     suspend fun getListProductsByCategory(
         @Query("category") category: Int,
         @Query("page") page: Int,
         @Query("orderby") orderBy: String,
-    ): ProductsDto
+    ): List<ProductsResponseItemDto>
 
     @GET("products/categories")
     suspend fun getListCategories(
         @Query("page") page: Int,
-    ): CategoriesDto
+    ): List<CategoriesItemDto>
 
     @GET("products")
     suspend fun searchProducts(
         @Query("search") querySearch: String,
         @Query("page") page: Int,
         @Query("orderby") orderBy: String,
-    ): ProductsDto
+    ): List<ProductsResponseItemDto>
 
     @GET("products/categories")
     suspend fun searchCategories(
         @Query("search") querySearch: String,
         @Query("page") page: Int,
-    ): CategoriesDto
+    ): List<CategoriesItemDto>
 
 
     @GET("products/{product_id}")
