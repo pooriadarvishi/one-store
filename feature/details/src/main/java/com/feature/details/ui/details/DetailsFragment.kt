d96fe96a285d860e40eaf5c58ff7658dd660dcab
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 23:32:56 +0330

    impl dataBinding for DetailsFragment

--- feature/details/src/main/java/com/feature/details/ui/details/DetailsFragment.kt
+++ feature/details/src/main/java/com/feature/details/ui/details/DetailsFragment.kt
@@ -6,27 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.feature.details.R
+import com.feature.details.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+private val detailsViewModel : DetailsViewModel by viewModels()
+    private lateinit var detailsBinding: FragmentDetailsBinding
 
-    companion object {
-        fun newInstance() = DetailsFragment()
-    }
-
-    private lateinit var viewModel: DetailsViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+
+        detailsBinding = FragmentDetailsBinding.inflate(inflater)
+        return detailsBinding.root
     }
 
 }
