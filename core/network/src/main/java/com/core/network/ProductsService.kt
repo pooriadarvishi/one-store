2fe73c52643e24d60ff240a552dd8095c299d2a6
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 22:30:16 +0330

    change return type service without Flow

--- core/network/src/main/java/com/core/network/ProductsService.kt
+++ core/network/src/main/java/com/core/network/ProductsService.kt
@@ -3,18 +3,17 @@ package com.core.network
 import com.core.network.model.category.CategoriesItemDto
 import com.core.network.model.details.ProductDetailsDto
 import com.core.network.model.products.ProductsResponseItemDto
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ProductsService {
     @GET("products")
-    fun getListProducts(
+    suspend fun getListProducts(
         @Query("page") page: Int,
         @Query("orderby") orderBy: String,
         @Query("order") order: String
-    ): Flow<List<ProductsResponseItemDto>>
+    ): List<ProductsResponseItemDto>
 
     @GET("products")
     fun getListProductsByCategory(
@@ -22,28 +21,28 @@ interface ProductsService {
         @Query("page") page: Int,
         @Query("orderby") orderBy: String,
         @Query("order") order: String
-    ): Flow<List<ProductsResponseItemDto>>
+    ): List<ProductsResponseItemDto>
 
     @GET("products/categories")
-    suspend fun getListCategories(
+    fun getListCategories(
         @Query("page") page: Int,
-    ): Flow<List<CategoriesItemDto>>
+    ): List<CategoriesItemDto>
 
     @GET("products")
-    suspend fun searchProducts(
+    fun searchProducts(
         @Query("search") querySearch: String,
         @Query("page") page: Int,
         @Query("orderby") orderBy: String,
         @Query("order") order: String
-    ): Flow<List<ProductsResponseItemDto>>
+    ): List<ProductsResponseItemDto>
 
     @GET("products/categories")
-    suspend fun searchCategories(
+    fun searchCategories(
         @Query("search") querySearch: String,
         @Query("page") page: Int,
-    ): Flow<List<CategoriesItemDto>>
+    ): List<CategoriesItemDto>
 
 
     @GET("products/{product_id}")
-    suspend fun getProductDetails(@Path("product_id") productId: Int): Flow<ProductDetailsDto>
+    fun getProductDetails(@Path("product_id") productId: Int): ProductDetailsDto
 }
