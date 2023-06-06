ea38323a337ae3170cb18852821bd712a12663b1
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 14:33:19 +0330

    add dispatchers into ProductNetworkDataSourceImpl

--- data/products/src/main/java/com/data/products/data/network/ProductNetworkDataSourceImpl.kt
+++ data/products/src/main/java/com/data/products/data/network/ProductNetworkDataSourceImpl.kt
@@ -4,41 +4,43 @@ import com.core.common.legacy.mappers.ProductDtoToProduct
 import com.core.common.legacy.mappers.map
 import com.core.common.model.models.products.ProductsItem
 import com.core.network.ProductsService
+import com.example.common_main.network.Dispatcher
+import com.example.common_main.network.KinDispatchers
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 class ProductNetworkDataSourceImpl(
     @Inject @Singleton private val productsService: ProductsService,
-    @Inject @Singleton private val productDtoToProduct: ProductDtoToProduct
+    @Inject @Singleton private val productDtoToProduct: ProductDtoToProduct,
+    @Inject @Singleton @Dispatcher(KinDispatchers.IO) private val ioDispatchers: CoroutineDispatcher,
+    @Inject @Singleton @Dispatcher(KinDispatchers.Default) private val defaultDispatchers: CoroutineDispatcher,
 ) : ProductNetworkDataSource {
     override suspend fun getListProducts(
-        page: Int,
-        orderBy: String,
-        order: String
-    ): List<ProductsItem> =
+        page: Int, orderBy: String, order: String
+    ): List<ProductsItem> = withContext(ioDispatchers) {
         productDtoToProduct.map(productsService.getListProducts(page, orderBy, order))
+    }
 
 
     override suspend fun getListProductsByCategory(
-        category: Int,
-        page: Int,
-        orderBy: String,
-        order: String
-    ): List<ProductsItem> = productDtoToProduct.map(
-        productsService.getListProductsByCategory(
-            category,
-            page,
-            orderBy,
-            order
+        category: Int, page: Int, orderBy: String, order: String
+    ): List<ProductsItem> = withContext(ioDispatchers) {
+        productDtoToProduct.map(
+            productsService.getListProductsByCategory(
+                category, page, orderBy, order
+            )
         )
-    )
-
+    }
 
     override suspend fun searchProducts(
-        querySearch: String,
-        page: Int,
-        orderBy: String,
-        order: String
-    ): List<ProductsItem> =
-        productDtoToProduct.map(productsService.searchProducts(querySearch, page, orderBy, order))
+        querySearch: String, page: Int, orderBy: String, order: String
+    ): List<ProductsItem> = withContext(ioDispatchers) {
+        productDtoToProduct.map(
+            productsService.searchProducts(
+                querySearch, page, orderBy, order
+            )
+        )
+    }
 }
