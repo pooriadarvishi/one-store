3fe9b849aea81b24cf9112819340f78cdece8b77
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 16:34:42 +0330

    impl hom fragment

---
+++ feature/home/src/main/java/com/feature/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.feature.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.feature.home.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        homeViewModel.getData()
+
+        return binding.root
+    }
+}
--- feature/home/src/main/java/com/feature/home/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.feature.home
-
-class MyClass {
-}
