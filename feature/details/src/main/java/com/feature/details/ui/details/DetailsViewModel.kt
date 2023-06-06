319efe2de9628a61eca542b24e3da74eb347e4aa
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-06 23:32:24 +0330

    impl DetailsViewModel

--- feature/details/src/main/java/com/feature/details/ui/details/DetailsViewModel.kt
+++ feature/details/src/main/java/com/feature/details/ui/details/DetailsViewModel.kt
@@ -1,7 +1,28 @@
 package com.feature.details.ui.details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.core.common.model.models.details.ProductDetails
+import com.data.products.data.repository.ProductRepository
+import com.example.common_main.result.Result
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailsViewModel : ViewModel() {
+class DetailsViewModel @Inject constructor(
+    private val productRepository: ProductRepository,
+) : ViewModel() {
 
+    private val _productDetails = MutableLiveData<Result<ProductDetails>>()
+    val productDetails: LiveData<Result<ProductDetails>> = _productDetails
+
+    private var productId: Int = 0
+    private fun getProductDetails() {
+        viewModelScope.launch {
+            productRepository.getProductDetails(productId).collect { ResultProductDetails ->
+                _productDetails.postValue(ResultProductDetails)
+            }
+        }
+    }
 }
