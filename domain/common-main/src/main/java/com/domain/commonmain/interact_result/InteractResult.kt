d1cb6115adce31cc39926bdf304e297b71294b45
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-07 14:34:20 +0330

    refactor interact Result

--- domain/common-main/src/main/java/com/domain/commonmain/interact_result/InteractResult.kt
+++ domain/common-main/src/main/java/com/domain/commonmain/interact_result/InteractResult.kt
@@ -2,23 +2,24 @@ package com.domain.commonmain.interact_result
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import java.util.concurrent.TimeUnit
 
 
-interface InteractResult<in P, R> {
-    operator fun invoke(params: P, timeoutMs: Long = defaultTimeoutMs): Flow<InteractResultState<R>> = flow {
-        emit(InteractResultState.Loading)
-        withTimeout(timeoutMs) {
-            emit(InteractResultState.Success(doWork(params)))
-        }
-    }.catch { emit(InteractResultState.Error) }
-
+abstract class InteractResult<P, Q> {
+    suspend operator fun invoke(
+        params: P,
+        timeoutMs: Long = defaultTimeoutMs
+    ): Flow<InteractResultState<Q>> =
+        doWork(params).map { q ->
+            InteractResultState.Success(q)
+        }.onStart { InteractResultState.Loading }
+            .catch { InteractResultState.Error }
 
     companion object {
         private val defaultTimeoutMs = TimeUnit.MINUTES.toMillis(5)
     }
 
-    suspend fun doWork(params: P): R
+    protected abstract suspend fun doWork(params: P): Flow<Q>
 }
