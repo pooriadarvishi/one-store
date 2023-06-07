3fa9ea12e31a2c2c831998cc6c9147108fb4750f
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-07 11:47:03 +0330

    impl domain gradle

--- domain/common-main/build.gradle.kts
+++ domain/common-main/build.gradle.kts
@@ -1,9 +1,45 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("com.google.dagger.hilt.android")
+    kotlin("android")
+    kotlin("kapt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+android {
+    namespace = "com.domain.commonmain"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    kapt {
+        correctErrorTypes = true
+    }
+}
+
+
+
+dependencies {
+    implementation(project(":data:category"))
+    implementation(project(":data:products"))
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 }
