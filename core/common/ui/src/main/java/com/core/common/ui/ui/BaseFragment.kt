ec1b9eafa6f6ae2d0ad93f8ff43e236737cc4050
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-11 16:13:54 +0330

    imple baseFragment

---
+++ core/common/ui/src/main/java/com/core/common/ui/ui/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.core.common.ui.ui
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+
+abstract class BaseFragment : Fragment() {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUI()
+    }
+
+    protected fun <T> observe(flow: StateFlow<T>, action: (T) -> Unit) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                flow.collect {
+                    action(it)
+                }
+            }
+        }
+    }
+
+
+    abstract fun setUI()
+
+
+    abstract fun bindLoading()
+
+    abstract fun bindSuccess()
+
+    abstract fun bindFail()
+}
