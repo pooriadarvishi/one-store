720b99324d7e24a946dd7b61fd981f7fdd64548d
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-11 16:17:05 +0330

    refactor DetailsViewModel with BaseViewModel

--- feature/details/src/main/java/com/feature/details/ui/details/DetailsViewModel.kt
+++ feature/details/src/main/java/com/feature/details/ui/details/DetailsViewModel.kt
@@ -1,13 +1,12 @@
 package com.feature.details.ui.details
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.core.common.model.models.details.ProductDetails
+import com.core.common.ui.ui.BaseViewModel
 import com.domain.commonmain.interact_result.InteractResultState
 import com.domain.commonmain.interactors.GetProductDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,12 +14,11 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailsViewModel @Inject constructor(
     private val getProductDetails: GetProductDetailsUseCase,
-) : ViewModel() {
+) : BaseViewModel<InteractResultState<ProductDetails>>() {
     private var productId: Int = 0
 
-    private val _productDetails =
+    override var _data =
         MutableStateFlow<InteractResultState<ProductDetails>>(InteractResultState.Loading)
-    val productDetails: StateFlow<InteractResultState<ProductDetails>> = _productDetails
 
     operator fun invoke() {
         getProductDetails()
@@ -29,10 +27,11 @@ class DetailsViewModel @Inject constructor(
     private fun getProductDetails() {
         viewModelScope.launch {
             getProductDetails(GetProductDetailsUseCase.Params(productId)).collect { ResultProductDetails ->
-                _productDetails.value = ResultProductDetails
+                _data.value = ResultProductDetails
             }
         }
     }
+
 
     fun setProductId(id: Int) {
         productId = id
