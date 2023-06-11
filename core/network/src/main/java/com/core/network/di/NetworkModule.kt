afa0ed3e7e635cb33b3355da4ac3818e3dcb318e
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-11 10:38:56 +0330

    remove Provide Strings into NetworkModule

--- core/network/src/main/java/com/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/core/network/di/NetworkModule.kt
@@ -3,10 +3,6 @@ package com.core.network.di
 import com.core.network.ProductsService
 import com.core.network.util.consts.Service
 import com.core.network.util.provideApiService
-import com.core.network.util.qualifier.BaseUrl
-import com.core.network.util.qualifier.ConsumerKey
-import com.core.network.util.qualifier.ConsumerSecret
-import com.core.network.util.qualifier.PerPage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,33 +12,13 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    @Provides
-    @Singleton
-    @BaseUrl
-    fun provideBaseUrl(): String = Service.BASE_URL
-
-    @Provides
-    @Singleton
-    @ConsumerKey
-    fun provideConsumerKey(): String = Service.CONSUMER_KEY_VALUE
-
-    @Provides
-    @Singleton
-    @ConsumerSecret
-    fun provideConsumerSecret(): String = Service.CONSUMER_SECRET_VALUE
-
-
-    @Provides
-    @Singleton
-    @PerPage
-    fun providePerPage(): String = Service.PER_PAGE_VAlUE
-
 
     @Provides
     @Singleton
@@ -51,16 +27,13 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideInterceptor(
-        @ConsumerKey consumerKey: String,
-        @ConsumerSecret consumerSecret: String,
-        @PerPage perpage: String,
     ): Interceptor = Interceptor { chain ->
         val url = chain.request()
             .url
             .newBuilder()
-            .addQueryParameter(Service.CONSUMER_KEY, consumerKey)
-            .addQueryParameter(Service.CONSUMER_SECRET, consumerSecret)
-            .addQueryParameter(Service.PER_PAGE, perpage)
+            .addQueryParameter(Service.CONSUMER_KEY, Service.CONSUMER_KEY_VALUE)
+            .addQueryParameter(Service.CONSUMER_SECRET, Service.CONSUMER_SECRET_VALUE)
+            .addQueryParameter(Service.PER_PAGE, Service.PER_PAGE_VAlUE)
             .build()
         val request = chain.request()
             .newBuilder()
@@ -87,18 +60,18 @@ object NetworkModule {
     ): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(httpLoggingInterceptor)
         .addInterceptor(interceptor)
+        .readTimeout(3,TimeUnit.MINUTES)
         .build()
 
 
     @Provides
     @Singleton
     fun provideRetrofit(
-        @BaseUrl baseUrl: String,
         gsonConverter: GsonConverterFactory,
         okHttpClient: OkHttpClient
     ): Retrofit = Retrofit
         .Builder()
-        .baseUrl(baseUrl)
+        .baseUrl(Service.BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(gsonConverter)
         .build()
