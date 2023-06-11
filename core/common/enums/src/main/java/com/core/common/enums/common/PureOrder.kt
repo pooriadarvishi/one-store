bca9f756939480cdbcfb2a44ee41ab2e12230fdf
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-06-11 10:35:29 +0330

    impl res package for feature

--- core/common/enums/src/main/java/com/core/common/enums/common/PureOrder.kt
+++
@@ -1,8 +0,0 @@
-package com.core.common.enums.common
-
-import com.core.common.enums.enums.OrderByFilter
-import com.core.common.enums.enums.OrderFilter
-
-
-fun pureOrder(orderFilter: OrderFilter): String = orderFilter.filterName
-fun pureOrder(orderByFilter: OrderByFilter): String = orderByFilter.filterName
--- core/network/src/main/java/com/core/network/util/qualifier/BaseUrl.kt
+++
@@ -1,7 +0,0 @@
-package com.core.network.util.qualifier
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class BaseUrl
